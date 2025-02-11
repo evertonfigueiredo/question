a64142cd9f61470390f1a389e88affdf4d8086f4
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2025-02-11 13:12:02 -0300

    Atualização de paginação na pesquisa

--- app/Http/Controllers/ResponseAnswerController.php
+++ app/Http/Controllers/ResponseAnswerController.php
@@ -15,42 +15,40 @@ class ResponseAnswerController extends Controller
     /**
      * Display a listing of the resource.
      */
-public function index(Request $request)
-{
-    $surveyId = $request->query('survey_id');
+    public function index(Request $request)
+    {
+        $surveyId = $request->query('survey_id');
 
-    if (!$surveyId) {
-        return redirect()->route('surveys.index'); // Redireciona para /surveys
+        if (!$surveyId) {
+            return redirect()->route('surveys.index'); // Redireciona para /surveys
+        }
+
+
+
+        $uniqueIds = ResponseAnswer::where('survey_id', $surveyId)->groupBy('unique_id')->paginate(5);
+
+        // Obter todas as respostas correspondentes aos unique_id da página atual
+        $responseAnswers = ResponseAnswer::whereIn('unique_id', $uniqueIds->pluck('unique_id'))
+            ->where('survey_id', $surveyId)
+            ->get();
+
+        // Agrupar as respostas pelo unique_id
+        $groupedByUniqueId = $responseAnswers->groupBy('unique_id')->map(function ($responses) {
+            $survey_id = $responses->first()->survey_id;
+            $questionCount = Question::where('survey_id', $survey_id)->count();
+            $answeredQuestionsCount = $responses->pluck('question_id')->unique()->count();
+
+            return [
+                'survey_id' => $survey_id,
+                'responses' => $responses,
+                'answeredQuestionsCount' => $answeredQuestionsCount,
+                'questionCount' => $questionCount
+            ];
+        });
+
+        return view('response-answer.index', compact('uniqueIds', 'groupedByUniqueId', 'surveyId'))
+            ->with('i', ($request->input('page', 1) - 1) * $uniqueIds->perPage());
     }
-
-    // Pegamos os unique_id distintos paginados
-    $uniqueIds = ResponseAnswer::where('survey_id', $surveyId)
-        ->select('unique_id') // Pegamos apenas o unique_id
-        ->distinct()
-        ->paginate(100); // Defina o número de registros por página
-
-    // Obter todas as respostas correspondentes aos unique_id da página atual
-    $responseAnswers = ResponseAnswer::whereIn('unique_id', $uniqueIds->pluck('unique_id'))
-        ->where('survey_id', $surveyId)
-        ->get();
-
-    // Agrupar as respostas pelo unique_id
-    $groupedByUniqueId = $responseAnswers->groupBy('unique_id')->map(function ($responses) {
-        $survey_id = $responses->first()->survey_id;
-        $questionCount = Question::where('survey_id', $survey_id)->count();
-        $answeredQuestionsCount = $responses->pluck('question_id')->unique()->count();
-
-        return [
-            'survey_id' => $survey_id,
-            'responses' => $responses,
-            'answeredQuestionsCount' => $answeredQuestionsCount,
-            'questionCount' => $questionCount
-        ];
-    });
-
-    return view('response-answer.index', compact('uniqueIds', 'groupedByUniqueId', 'surveyId'))
-        ->with('i', ($request->input('page', 1) - 1) * $uniqueIds->perPage());
-}
 
 
 
@@ -76,10 +74,10 @@ public function index(Request $request)
         if ($responseAnswers->isEmpty()) {
             abort(404, 'Nenhuma resposta encontrada para este ID.');
         }
-    
+
         return view('response-answer.show', compact('responseAnswers', 'unique_id'));
     }
-    
+
 
     /**
      * Show the form for editing the specified resource.
