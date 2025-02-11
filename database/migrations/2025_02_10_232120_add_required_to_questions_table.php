2266ab020675b40f123d4386eda34a6ba4ce21b1
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2025-02-10 21:20:09 -0300

    atualização

--- database/migrations/2025_02_10_232120_add_required_to_questions_table.php
+++ database/migrations/2025_02_10_232120_add_required_to_questions_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('required');
             $table->boolean('required')->default(true);
         });
     }
